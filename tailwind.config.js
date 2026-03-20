68b6b28b95a27de03516005eb1e132f693cd7ded
nguyenthai0610 <nguyenthaigha00@gmail.com>
2026-03-20 17:47:27 +0700

    up

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Điều chỉnh đường dẫn theo dự án
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

7450c1ab02d2ce34d5feff61d9f0b80c6ea077fb
nguyenthai0610 <nguyenthaigha00@gmail.com>
2026-03-24 00:49:02 +0700

    up

--- font-end-template/asset/js/main.js
+++ font-end-template/asset/js/main.js
@@ -9,3 +9,17 @@ const menuBtn = document.getElementById('menu-btn');
         closeBtn.addEventListener('click', () => {
             navMenu.classList.remove('active');
         });
+
+
+
+        function startCountdown() {
+            let h = 2, m = 45, s = 0;
+            setInterval(() => {
+                if (s > 0) s--;
+                else { if (m > 0) { m--; s = 59; } else { if (h > 0) { h--; m = 59; s = 59; } } }
+                document.getElementById('hours').innerText = String(h).padStart(2, '0');
+                document.getElementById('minutes').innerText = String(m).padStart(2, '0');
+                document.getElementById('seconds').innerText = String(s).padStart(2, '0');
+            }, 1000);
+        }
+        startCountdown();
